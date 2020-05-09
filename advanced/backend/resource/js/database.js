5ba161778ca44bd69822fa7b2c332db03335e3fe
Stephen Kang <ki6080@gmail.com>
2020-05-09 18:05:42 +0900

    Hide db config from repo

--- advanced/backend/resource/js/database.js
+++ advanced/backend/resource/js/database.js
@@ -1,8 +1,4 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '1q2w3e4r',
-  port : 3031,
-  database : 'my_db'
-});
+var fs         = require('fs');
+var connection = mysql.createConnection(
+    JSON.parse(fs.readFileSync('.connection.json')));
